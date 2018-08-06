ab37b145aff346ae2f9667ab81843c335d4d1c45
ishwaryachitsen0523 <ishwaryachitsen98@gmail.com>
2018-08-06 14:30:19 +0530

    Create aba.java

---
+++ aba.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Sample
+{
+public static void main(String args[])
+{
+char a;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the input");
+if(a>='a' && a>='z' || a>='A' && a>='Z')
+{
+System.out.println("alphabet");
+}
+else
+{
+System.out.println("not alphabet");
+}
+}
+}
