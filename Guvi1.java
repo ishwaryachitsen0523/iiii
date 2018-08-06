3d0e33ac5dc2c5b03d698239af5e0941b639f4e5
ishwaryachitsen0523 <ishwaryachitsen98@gmail.com>
2018-08-06 14:39:45 +0530

    Create Guvi1.java

---
+++ Guvi1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Guvi1 {
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+	Scanner a =new Scanner (System.in);
+		System.out.println("enter any three no");
+		int b=a.nextInt();
+		int c=a.nextInt();
+		int d=a.nextInt();
+		if((b>=c)&&(b>=d)) {
+			System.out.println(b);
+		}
+		if((c>=b)&&(c>=d)) {
+			System.out.println(c);
+		}
+		if((d>=b)&&(d>=c)) {
+			System.out.println(d);
+		}
+		
+		}
+    }
